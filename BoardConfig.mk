6d31222290f8c550e6b37b6b9ce8a9e482614971
Michael Webster <miketwebster@gmail.com>
2010-12-18 09:07:49 -0500

    Gingerbeard

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,12 +23,15 @@
 # WARNING: This line must come *before* including the proprietary
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
-USE_CAMERA_STUB := true
+USE_CAMERA_STUB := false
+BOARD_USE_FROYO_LIBCAMERA := true
 
 # inherit from the proprietary version
 -include vendor/motorola/sholes/BoardConfigVendor.mk
 
-TARGET_NO_BOOTLOADER := true
+# use pre-kernel.35 vold usb mounting
+BOARD_USE_USB_MASS_STORAGE_SWITCH := true
+
 
 TARGET_BOARD_PLATFORM := omap3
 
@@ -39,6 +42,8 @@ TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
 
 TARGET_BOOTLOADER_BOARD_NAME := sholes
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 3200
 
 # Wifi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
@@ -69,9 +74,9 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 BOARD_HAS_SMALL_RECOVERY := true
 
-TARGET_RECOVERY_UI_LIB := librecovery_ui_sholes librecovery_ui_generic
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_sholes librecovery_ui_generic
 
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_generic
+#TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_generic
 
 USE_SHOLES_PROPERTY := true
 TARGET_PREBUILT_KERNEL := device/motorola/sholes/kernel
--- sholes.mk
+++ sholes.mk
@@ -65,14 +65,23 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # media config xml file
 PRODUCT_COPY_FILES += \
     device/motorola/sholes/media_profiles.xml:system/etc/media_profiles.xml
 
 PRODUCT_PACKAGES += \
-    librs_jni
+    librs_jni \
+    tiwlan.ini \
+    dspexec \
+    libbridge \
+    overlay.omap3 \
+    wlan_cu \
+    libtiOsLib \
+    wlan_loader \
+    libCustomWifi \
 
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
