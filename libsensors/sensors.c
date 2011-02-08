00c21b48dd92c122bee084931f51043b9b1bc7e9
nadlabak <pavel@doshaska.net>
2011-02-08 19:57:16 +0100

    sholes: fix lightsensor range; ignore lightsensor when flashlight is on
    Change-Id: Ib5c853bcce393628681569185faeff5fe70fa7af

--- libsensors/sensors.c
+++ libsensors/sensors.c
@@ -52,7 +52,7 @@ static const struct sensor_t sSensorList[] = {
         { "LM3530 Light sensor",
                 "National Semiconductor",
                 1, SENSORS_HANDLE_BASE+ID_L,
-                SENSOR_TYPE_LIGHT, 1400.0f, 1.0f, 0.5f, 0, { } },
+                SENSOR_TYPE_LIGHT, 27000.0f, 1.0f, 0.5f, 0, { } },
 };
 
 static int open_sensors(const struct hw_module_t* module, const char* name,
