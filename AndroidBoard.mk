ec2a00189f74ab364121b5b766b0ba8ee521a20d
Austen Dicken <cvpcsm@gmail.com>
2011-03-08 13:53:07 -0600

    added keychar symlink to quiet down sholes logs
    sholes continually looks for qtouch-touchscreen which should be
    a symlink to qwerty, so just make sure that's there.
    
    Change-Id: I5da3d353c6359ff88b7a9d221473485713e8bf7c

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -22,5 +22,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+# Symlink keysym file that sholes looks for
+file := $(TARGET_OUT)/usr/keychars/qtouch-touchscreen.kcm.bin
+ALL_PREBUILT += $(file)
+$(file) : $(TARGET_OUT)/usr/keychars/qwerty.kcm.bin
+	@echo "Symlink: $@ -> qwerty.kcm.bin"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf qwerty.kcm.bin $@
+
 # include the non-open-source counterpart to this file
 -include vendor/motorola/sholes/AndroidBoardVendor.mk
