888c535a58162f8aa4a5b0fd6d82ff7925cf668a
Peter Alfonso <peteralfonsojr@gmail.com>
2011-09-26 09:47:34 -0400

    Remove common properties, these are set in my vendor setup

--- sholes.mk
+++ sholes.mk
@@ -32,14 +32,8 @@ $(call inherit-product-if-exists, vendor/motorola/sholes/sholes-vendor.mk)
 
 ## (3)  Finally, the least specific parts, i.e. the non-CDMA-specific aspects
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.com.android.wifi-watchlist=GoogleGuest \
 	ro.error.receiver.system.apps=com.google.android.feedback \
-	ro.setupwizard.enterprise_mode=1 \
-	ro.com.google.clientidbase=android-verizon \
 	ro.com.google.locationfeatures=1 \
-	ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-	ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-	ro.setupwizard.mode=OPTIONAL \
 	ro.cdma.home.operator.numeric=310004 \
 	ro.cdma.home.operator.alpha=Verizon \
 	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
