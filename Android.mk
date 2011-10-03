LineageOS/android_device_motorola_sholes

sholes: Only inherit makefiles if building the device
This avoids generating device-specific files on other
builds.

Change-Id: I3294812232cc4e02da7471bb0e72b75a1e8d7267

Android.mk
@@ -1,0 +1,3 @@
+ifeq ($(TARGET_DEVICE),sholes)
+    include $(all-subdir-makefiles)
+endif

peteralfonso/platform_device_motorola_sholes

Android.mk
@@ -1,0 +1,3 @@
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),sholes)
+    include $(all-subdir-makefiles)
+endif